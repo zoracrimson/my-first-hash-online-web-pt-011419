3b132129115a2c7277fa8445335980313b2ee46a
Kat Maldon <katharinamaldon@gmail.com>
2019-01-26 18:22:23 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -11,9 +11,9 @@ def shipping_manifest
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
   shipping_manifest = {
-    "whale bone corsets" => "5",
-    "porcelain vases" => "2",
-    "oil paintings" => "3"
+    "whale bone corsets" => 5,
+    "porcelain vases" => 2,
+    "oil paintings" => 3
   }
 end
 

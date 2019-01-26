1c03e4684ec814dcb9025b8c03bd15930921ca96
Kat Maldon <katharinamaldon@gmail.com>
2019-01-26 18:18:39 +0000

    Automatically backed up by Learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -1,7 +1,8 @@
 def my_hash
   # use the literal constructor to set the variable, my_hash, equal to a hash with key/value pairs of your choice.
-
-  
+  my_hash => {
+    "name" => "Kat"
+  }
 end
 
 
